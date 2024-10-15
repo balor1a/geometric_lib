7ed4e4b492ae164087105341095fde3b787ae281
andrey <andreyklim06@gmail.com>
2024-10-15 20:58:45 +0300

    added documentation

--- circle.py
+++ circle.py
@@ -2,9 +2,12 @@ import math
 
 
 def area(r):
+    '''принимает радиус круга возвращает его площадь'''
+
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''принимает радиус круга возвращает его длину окружности'''
     return 2 * math.pi * r
 
--- rectuagle.py
+++ rectuagle.py
@@ -1,5 +1,7 @@
 def area(a, b): 
+    '''принимает стороны a  и  b и возвращает площадь'''
     return a * b 
 
-def perimeter(a, b): 
+def perimeter(a, b):
+    '''принимает стороны a и b и возвращает периметр'''
     return a * 2 + b * 2 
